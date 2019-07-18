2d23f747ddff6939d586d025425f59736f729c1e
Oleksandr Dudak <Oleksandr_Dudak@epam.com>
2019-07-18 19:44:33 +0300

    initial

---
+++ src/main/java/me/tst/ostst/OststApplication.java
@@ -0,0 +1,13 @@
+package me.tst.ostst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OststApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OststApplication.class, args);
+	}
+
+}
